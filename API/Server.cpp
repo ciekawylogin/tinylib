148d497eb362fa15e1ae005f6d41a63e04e53163
kjastrzebski <krzysiekj8@gmail.com>
2014-06-07 21:50:06 +0200

    push

--- API/Server.cpp
+++ API/Server.cpp
@@ -17,6 +17,37 @@ void Server::listenSync()
 
 }
 
+void Server::addToBlackList(std::string ip)
+{
+    blackList.push_back(IP(ip));
+}
+
+ void Server::addToWhiteList(std::string ip)
+ {
+    whiteList.push_back(IP(ip));
+ }
+
+ void Server::checkBlackList(std::string ip)
+ {
+     IP x = IP(ip);
+     for(IP x: blackList)
+     {
+         if(std::string(x.address) == std::string(x.address))
+             return true;
+     }
+     return false;
+ }
+
+ void Server::checkWhiteList(std::string ip)
+ {
+     IP x = IP(ip);
+     for(IP x: whiteList)
+     {
+         if(std::string(x.address) == std::string(x.address))
+             return true;
+     }
+     return false;
+ }
 void Server::listenAsync()
 {
     if(listener == nullptr)
@@ -34,4 +65,3 @@ void Server::listenAsync()
     });
     */
 }
-
