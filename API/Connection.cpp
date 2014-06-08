b2b1eaa23dd8358fd49960fee1419a4b612266c0
mkrawczak <michalarturkrawczak@gmail.com>
2014-06-08 00:41:06 +0000

    Hmmm

--- API/Connection.cpp
+++ API/Connection.cpp
@@ -8,16 +8,16 @@ Connection::Connection(Socket socket)
     state = ConnectionState::CREATED;
 }
 
-AsyncOperation Connection::writeAsync(char *data, int dataSize, EventListener success, EventListener failure)
+AsyncOperation* Connection::writeAsync(char *data, int dataSize, EventListener success, EventListener failure)
 {
     WritingAction action(socket, data, dataSize, success, failure, listener_call_thread);
     writing_thread.add(action);
-    return AsyncOperation(action);
+    return new AsyncOperation(action);
 }
 
-AsyncOperation Connection::readAsync(char *data, int dataSize, EventListener success, EventListener failure)
+AsyncOperation* Connection::readAsync(char *data, int dataSize, EventListener success, EventListener failure)
 {
     ReadingAction action(socket, data, dataSize, success, failure, listener_call_thread);
     reading_thread.add(action);
-    return AsyncOperation(action);
+    return new AsyncOperation(action);
 }
